9ffee85c861831d0a93bb4ba2ffcc6fcd1d966f7
Talhah <talhah-dev@proton.me>
2023-06-05 10:44:25 +0300

    Working program

--- dots.py
+++ dots.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import shutil
 import argparse
 
 # Specify dir where all themes are located
@@ -9,7 +10,7 @@ import argparse
 # └── themes
 #    ├── dracula
 #    └── gruvbox
-theme_dir = "/home/talhah/themes/"
+theme_dir = "/home/talhah/Projects/desktop-env/themes"
 theme = ""
 
 parser = argparse.ArgumentParser()
@@ -17,6 +18,7 @@ parser.add_argument("-d","--dir",help="Path to directory containing themes")
 parser.add_argument("-t","--theme",help="Name of theme")
 args = parser.parse_args()
 
+# Setting up variables
 if args.dir != None:
     theme_dir = args.dir
 
@@ -42,8 +44,28 @@ else:
             theme = ""
             while theme not in map(str, range(1, len(themes) + 1)):
                 theme = input(input_message)
+            theme = themes[int(theme)-1]
     else:
         if not os.path.exists(os.path.join(theme_dir,theme)):
             print("Given theme does not exist, please try again")
             sys.exit(1)
-        os.chdir(theme)
+
+    os.chdir(theme)
+    user_path = os.path.expanduser("~/.config/")
+    cwd = os.getcwd() + "/"
+    for item in os.listdir():
+        try:
+            os.symlink(cwd + item,user_path + item)
+        except FileExistsError:
+            print(f"There is already a configuration for {item}")
+            user_input = ""
+            while user_input not in ["y","n","Y","N"]:
+                user_input = input("Do you want to delete current config? (y/n) ")
+            if user_input.lower() == "y":
+                if os.path.isfile(user_path + item):
+                    os.remove(user_path + item)
+                else:
+                    shutil.rmtree(user_path + item)
+                os.symlink(cwd + item,user_path + item)
+            else:
+                print(f"Skipping config for {item}")
